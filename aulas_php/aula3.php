61cfa082d8d3a27d114556ddfff334f08e16c0b8
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-17 12:33:33 -0300

    uso de arrays (vetor)

---
+++ aulas_php/aula3.php
@@ -0,0 +1,14 @@
+<?php
+//Uso de Arrays, que é tipo um vetor, so que não armazena só numeros
+//podendo armazenar tambem caracteres
+
+    $nome = array('duda','ryan','malu','cila');
+
+
+//dentro da variavel nome eu tenho todos esses nomes como informação
+    
+echo $nome[0];
+//acessei o indice na posição 0, que tem como valor 'duda"
+
+
+
