9d92f2fcfe977d3ee499f9ff900cc5bf8f2e8647
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-17 11:56:54 -0300

    Foi definido a diferença de variavel para constante

---
+++ aula2.php
@@ -0,0 +1,9 @@
+<?php
+// variáveis podem se alterar durante o codigo
+// mas podem ocorrer situações que pode surgir o desejo de atribuir um valor definitivo para uma variavel, 
+//então deixa de ser uma variavel e se torna uma constante
+// se declarar o valor de uma constante, ele não pode mais ser alterado
+
+    define ('nome', 'maria eduarda')
+//primeiro o nome da constante, depos a virgula que substitui o = e logo o valor da constante
+?>
