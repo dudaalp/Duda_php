52dbdab691cdde6de958c55c864a2d6052e71934
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-17 11:36:15 -0300

    adicionei todas as váriaveis propostas pelo professor e especifiquei cada uma

--- aula1.php
+++ aula1.php
@@ -2,5 +2,26 @@
 $nome = 'Maria Eduarda';
 echo 'Meu nome é: '; 
 echo $nome;
-echo $nome;
+echo "<br>";
+
+//echo=imprimir na tela
+
+echo 'meu nome é: ' . $nome;
+
+ // precisa do ponto para ligar informações
+ //abaixo as variáveis
+
+$nome = 'nome'; //armazena texto
+$idade = 2 ; //armazena numeros
+$bool = true; //armazena informações verdadeiras ou falsas
+$double = 2.4; //armazena numeros quebrados
+
+//se eu colocar o conteudo da variavel entre aspas duplas ou simples, 
+//o conteudo imediatamente vita do tipo texto, me atentar a a isso
+
+//se eu colocar pra imprimir na tela a variável bool com valor true, vai aparecer "1" 
+//pois o valor de true é 1 e false é 0
+
+
+
 
