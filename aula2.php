7ac7b904004a077879246a9066ac1621ba0128c3
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-17 12:14:41 -0300

    acabei a matéria constante

--- aula2.php
+++ aula2.php
@@ -4,7 +4,10 @@
 //então deixa de ser uma variavel e se torna uma constante
 // se declarar o valor de uma constante, ele não pode mais ser alterado
 
-    define ('NOME', 'maria eduarda')
+    define ('NOME', 'maria eduarda');
 //primeiro o nome da constante, depos a virgula que substitui o = e logo o valor da constante
 //O NOME DA CONSTANTE TEM QUE SER EM MAIUSCULO
+
+    echo NOME;
+//imprimiu na tela o valor de NOME
 ?>
