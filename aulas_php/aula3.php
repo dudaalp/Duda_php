2ef9caba01b17c2f295b4600febcedc32ce2d837
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-17 12:41:14 -0300

    jeitos de declarar arrays

--- aulas_php/aula3.php
+++ aulas_php/aula3.php
@@ -10,5 +10,13 @@
 echo $nome[0];
 //acessei o indice na posição 0, que tem como valor 'duda"
 
+$nome1 [] = 'maria';
+//estou dizendo que na posição 0 o valor é 'maria'
+//que é atribuido automaticamente a posição 0, no caso a primeira posição
 
+$nome2[] = 'joão';
+//estou dizendo que na posição 0 o valor é 'joão'
+//que é atribuido automaticamente a posição 1, no caso a segunda posição
 
+$nome7[7] = 'Joana';
+//atribui o nome Joana a posição 7
