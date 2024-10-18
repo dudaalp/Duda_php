7e622f9abeed006edbba50d03f54e4ca0249bdcc
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-18 08:50:56 -0300

    mesma coisa de operações em C

--- aulas_php/aula5.php
+++ aulas_php/aula5.php
@@ -1,1 +1,22 @@
 <?php
+
+$n1=10; 
+
+$nn1=2.5+2.5;
+
+$n2=$n1-$nn1;
+
+$nn2=$n1/2;
+
+$n3= 2*2;
+
+echo $nn1;
+echo '<br>';
+echo $n2; 
+echo '<br>';
+echo $nn2;
+echo '<br>';
+echo $n3;
+echo '<br>';
+
+//tudo igual a C, módulo (%) também
