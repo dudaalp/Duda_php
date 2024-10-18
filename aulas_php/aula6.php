3b81399091fcb51ca176edec6003aff77dfeb8f0
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-18 09:54:45 -0300

    iniciando condições

---
+++ aulas_php/aula6.php
@@ -0,0 +1,22 @@
+<?php
+$v1= 'duda';
+$v2= 'maria';
+
+if($v1 == $v2){
+    echo '<div style="backgroud-color:red"> Nomes iguais </div>';
+}else{
+    echo 'A condição não bate';
+}
+echo '<br>';
+?>
+
+<?php
+$v3= 'duda';
+$v4= 'duda';
+
+if($v3 == $v4){
+    echo '<div> Nomes iguais </div>';
+}else{
+    echo 'A condição não bate';
+}
+?>
