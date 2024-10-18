c74df33400e143fca871e0d3db0957260848c9ba
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-18 12:39:41 -0300

    foreach

---
+++ aulas_php/aula8.php
@@ -0,0 +1,11 @@
+<?php
+
+$nomes = ['Maria', 'Luis', 'Felipe', 'Wesley'];
+
+// echo $nomes[1];
+
+
+foreach ($nomes as $key => $nome) {
+    echo $key . ' ' . $nome . '<br>';
+}
+//vai percorrer todo o vetor imprimindo todos os nomes () não é uma condição
