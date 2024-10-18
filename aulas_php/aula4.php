a940dfba437ce13ad090bb5cbd4d867b54aea9d2
Maria Eduarda <mariaeduarda11alveslima15@gmail.com>
2024-10-18 08:31:39 -0300

    na aula 5 começando operações matematicas

--- aulas_php/aula4.php
+++ aulas_php/aula4.php
@@ -1,1 +1,9 @@
 <?php
+
+$nome = 'maria';
+echo 'olá meu nome é: '.$nome; 
+echo '<br>'; //tem que imprimir a quebra de linha
+//concatenação é unir uma sentença/palavra a uma variavel com um ponto -> .
+
+echo "meu nome é $nome"; //quando são aspas duplas ele reconhece a variavel sem precisar de ponto
+?>
